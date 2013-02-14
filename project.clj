dfb3de052f225654b4e518ff27ccd92074a1120c
Hugo Duncan <hugo@hugoduncan.org>
2013-02-14 12:20:34 -0500

    Use leiningen instead of maven
    Moves the crate build to leiningen.  Adds a release script and a logback
    configuration.

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject com.palletops/java-crate "0.8.0-SNAPSHOT"
+  :description "Pallet crate to install, configure and use java"
+  :url "http://palletops.com"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:url "git@github.com:pallet/java-crate.git"}
+
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.palletops/pallet "0.8.0-beta.1"]]
+  :repositories {"sonatype"
+                 {:url "https://oss.sonatype.org/content/repositories/releases/"
+                  :snapshots false}})
