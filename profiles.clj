2510542cf6b530ba0ecfabaf729bc959f42a7125
Hugo Duncan <hugo@hugoduncan.org>
2013-04-15 14:38:54 -0400

    Add doc plugins to profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -3,7 +3,9 @@
         [com.palletops/crates "0.1.0"]
         [ch.qos.logback/logback-classic "1.0.9"]]
        :plugins [[lein-set-version "0.3.0"]
-                 [lein-resource "0.3.2"]]
+                 [lein-resource "0.3.2"]
+                 [codox/codox.leiningen "0.6.4"]
+                 [lein-marginalia "0.7.1"]]
        :aliases {"live-test-up"
                  ["pallet" "up" "--phases" "settings,configure,test"]
                  "live-test-down" ["pallet" "down"]
