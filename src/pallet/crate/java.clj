fb321cb4d96818f620623bf90b4048f2ef6dd454
Hugo Duncan <hugo@hugoduncan.org>
2013-05-16 10:12:29 -0400

    Fix :settings phase in server-spec

--- src/pallet/crate/java.clj
+++ src/pallet/crate/java.clj
@@ -505,5 +505,5 @@ http://www.webupd8.org/2012/01/install-oracle-java-jdk-7-in-ubuntu-via.html"
   [settings & {:keys [instance-id] :as options}]
   (api/server-spec
    :phases {:settings (plan-fn
-                        (apply-map pallet.crate.java/settings settings options))
+                        (pallet.crate.java/settings (merge settings options)))
             :install (plan-fn (install options))}))
